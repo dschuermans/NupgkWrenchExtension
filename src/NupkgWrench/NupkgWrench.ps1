a4122b4ab0dd26357f39e63960c7c061b7209472
Dirk Schuermans <d.schuermans@scia.net>
2018-04-19 17:10:13 +0200

    Initial commit, v0.0.4

---
+++ src/NupkgWrench/NupkgWrench.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [string]$NupkgWrench,
+    [string]$Command,
+    [string]$CommandArguments,
+    [string]$CommandOptions,
+    [string]$SubCommand
+)
+
+begin
+{
+    Write-Host "# Settings:"
+    Write-Host "    NupkgWrench: $NupkgWrench"
+    Write-Host "    Command: $Command"
+    
+    if($SubCommand){
+        Write-Host "    Sub-Command: $SubCommand"
+    }
+
+    Write-Host "    Arguments: $CommandArguments"
+    Write-Host "    Options: $CommandOptions"
+}
+
+process
+{
+    $combined = $CommandArguments.Split(' ') + $CommandOptions.Split(' ')
+
+    if($SubCommand) {
+        Write-Verbose "Executing command: $Command $SubCommand $CommandArguments $CommandOptions"    
+        & $NupkgWrench $Command $SubCommand $combined
+    } else {
+        Write-Verbose "Executing command: $Command $CommandArguments $CommandOptions"
+        & $NupkgWrench $Command $combined
+    }
+}
+
+end
+{
+
+}
